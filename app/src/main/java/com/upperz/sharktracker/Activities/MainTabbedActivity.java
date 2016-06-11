263c25acd2107724586a73833f214701dd442643
Derek Duncan <derekgduncan@gmail.com>
2016-06-11 15:51:39 -0700

    return commit

--- app/src/main/java/com/upperz/sharktracker/Activities/MainTabbedActivity.java
+++ app/src/main/java/com/upperz/sharktracker/Activities/MainTabbedActivity.java
@@ -16,8 +16,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.InterstitialAd;
 import com.upperz.sharktracker.Fragments.ListFragment;
 import com.upperz.sharktracker.Fragments.MapFragment;
 import com.upperz.sharktracker.Fragments.SponsorFragment;
@@ -31,16 +29,11 @@ public class MainTabbedActivity extends AppCompatActivity {
     public ListFragment lf;
     public SponsorFragment sf;
 
-    private InterstitialAd mInterstitialAd;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_tabbed);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
-        loadAd();
 
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -70,9 +63,6 @@ public class MainTabbedActivity extends AppCompatActivity {
                         lf.trackName = null;
                     }
                 }
-
-                if(mInterstitialAd.isLoaded())
-                    mInterstitialAd.show();
             }
 
             @Override
@@ -205,19 +195,4 @@ public class MainTabbedActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    public void loadAd()
-    {
-        mInterstitialAd = new InterstitialAd(this);
-        mInterstitialAd.setAdUnitId("ca-app-pub-6091505442457709/4923967670");
-
-        AdRequest adRequest = new AdRequest.Builder()
-                .build();
-
-        mInterstitialAd.loadAd(adRequest);
-    }
-
-
-
-
 }
